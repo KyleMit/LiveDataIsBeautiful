147eb1623d4e8c5e70fa0e5d247a7c7dfc1641c6
Kyle Mitofsky <KyleMit@gmail.com>
2014-12-18 00:07:36 -0500

    Added Campaign Contributions

---
+++ CampaignContributions/data.js
@@ -0,0 +1,48 @@
+﻿var data = (function() {
+    var url = "http://transparencydata.com/api/1.0/contributions.json";
+    var recipient = "Patrick Leahy";
+    var cycle = 2014;
+    var apiKey = "49cc54a8a3874ecf96f21af6ed96264a";
+    var debug = true;
+
+
+    var massageData = function(rawData) {
+        if (debug) console.log(rawData);
+
+        var simplifiedData = rawData.map(function (item) {
+            return {
+                amount: item.amount,
+                contributor_name: item.contributor_name,
+                contributor_state: item.contributor_state,
+                date: item.date
+            };
+        });
+
+        var series = Enumerable.From(simplifiedData)
+            .GroupBy("$.contributor_state", null,
+                function (key, g) {
+                    return {
+                        "hc-key": "us-" + key.toLowerCase(),
+                        value: g.Max("$.amount")
+                    }
+                })
+            .ToArray();
+
+        loadChart(series);
+    };
+
+    //http://sunlightlabs.github.io/datacommons/search.html#methods/campaign-contributions
+    $.ajax({
+        dataType: "jsonp",
+        url: url,
+        data: {
+            amount: ">|100",
+            recipient_ft: recipient,
+            cycle: cycle,
+            apikey: apiKey
+        },
+        success: massageData
+    });
+
+
+})();
---
+++ CampaignContributions/script.js
@@ -0,0 +1,48 @@
+﻿function loadChart(data) {
+
+    $(function () {
+
+        $('#container').highcharts("Map", {
+            title: {
+                text: 'Leahy Campaign Contributions by State - 2014'
+            },
+            subtitle: {
+                text: 'Source: /u/livedataisbeautiful',
+                align: 'right'
+            },
+            credits: {
+                text: 'github.com/LiveDataIsBeautiful',
+                href: 'https://github.com/KyleMit/LiveDataIsBeautiful/tree/gh-pages/CampaignContributions'
+            },
+            mapNavigation: {
+                enabled: true,
+                buttonOptions: {
+                    verticalAlign: 'bottom'
+                }
+            },
+            colorAxis: {
+                min: 0,
+                max: 3000
+            },
+            series: [{
+                data: data,
+                mapData: Highcharts.maps['countries/us/us-all'],
+                joinBy: 'hc-key',
+                animation: true,
+                name: 'Campaign Contributions',
+                states: {
+                    hover: {
+                        color: '#BADA55'
+                    }
+                },
+                dataLabels: {
+                    enabled: true,
+                    format: '{point.name}'
+                },
+                tooltip: {
+                    valuePrefix: '$'
+                }
+            }]
+        });
+    });
+}
